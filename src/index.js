ebd94180e4540cefdce30ec34fb5092c805b8f62
cleytonmadruga <cleytonhonda887@gmail.com>
2024-03-19 16:31:19 +0000

    meu segundo progeto

---
+++ src/index.js
@@ -0,0 +1,25 @@
+let userName = "Mega dinosauro"
+   let pontosDeXp = "5000"
+    
+   if (pontosDeXp < 1000){
+        nivelDoHeroi = "ferro"
+
+   }else if (pontosDeXp >= 1000 && pontosDeXp <= 2000){
+       nivelDoHeroi = "Bronze"
+
+   }else if (pontosDeXp >= 2001 && pontosDeXp <= 3000){
+       nivelDoHeroi = "Prata"  
+
+   }else if (pontosDeXp >= 3001 && pontosDeXp <= 4000){
+       nivelDoHeroi = "Ouro"
+
+   }else if (pontosDeXp >= 4001 && pontosDeXp <= 5000){
+       nivelDoHeroi = "Diamante"
+
+   }else if (pontosDeXp >= 5001 && pontosDeXp <= 6000){
+
+   }
+   console.log(`o heroi ${userName} ésta no nivel ${nivelDoHeroi}`);
+    
+
+
